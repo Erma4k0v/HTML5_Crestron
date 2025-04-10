5d6c6068cc49e3e2837d9fbbe45eded2961b07c1
Lev <Levermackov78390@gmail.com>
2025-04-10 17:46:30 +0300

    webpack

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import {
+    publishEvent,
+    subscribeState,
+    bridgeReceiveIntegerFromNative,
+    bridgeReceiveBooleanFromNative,
+    bridgeReceiveStringFromNative,
+    bridgeReceiveObjectFromNative
+} from '@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib';
+ 
+window.bridgeReceiveIntegerFromNative = bridgeReceiveIntegerFromNative;
+window.bridgeReceiveBooleanFromNative = bridgeReceiveBooleanFromNative;
+window.bridgeReceiveStringFromNative = bridgeReceiveStringFromNative;
+window.bridgeReceiveObjectFromNative = bridgeReceiveObjectFromNative;
